4381540ee8399fd9b671aaddd466f779a54685c9
Joao Marcos <joaomarcosgol2000@gmail.com>
2024-10-13 19:18:01 -0300

    Criando o serviço de gerenciamento de Veiculo

---
+++ src/main/java/br/uespi/autoescola/service/VeiculoService.java
@@ -0,0 +1,31 @@
+package br.uespi.autoescola.service;
+
+import br.uespi.autoescola.model.Veiculo;
+import br.uespi.autoescola.repository.VeiculoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VeiculoService {
+
+    @Autowired
+    private VeiculoRepository veiculoRepository;
+
+    public List<Veiculo> findAll() {
+        return veiculoRepository.findAll();
+    }
+
+    public Veiculo findById(Long id) {
+        return veiculoRepository.findById(id).orElse(null);
+    }
+
+    public Veiculo save(Veiculo veiculo) {
+        return veiculoRepository.save(veiculo);
+    }
+
+    public void deleteById(Long id) {
+        veiculoRepository.deleteById(id);
+    }
+}
