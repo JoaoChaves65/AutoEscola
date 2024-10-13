ae7800b3b3980e1198f74cd7c75dbe422bcceb04
Joao Marcos <joaomarcosgol2000@gmail.com>
2024-10-13 18:51:27 -0300

    Criando o serviço de gerenciamento de Aluno

---
+++ src/main/java/br/uespi/autoescola/service/AlunoService.java
@@ -0,0 +1,31 @@
+package br.uespi.autoescola.service;
+
+import br.uespi.autoescola.model.Aluno;
+import br.uespi.autoescola.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public List<Aluno> findAll() {
+        return alunoRepository.findAll();
+    }
+
+    public Aluno findById(Long id) {
+        return alunoRepository.findById(id).orElse(null);
+    }
+
+    public Aluno save(Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    public void deleteById(Long id) {
+        alunoRepository.deleteById(id);
+    }
+}
