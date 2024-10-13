e28010cf894d5b2da5475e770ef3dbe823c60fbd
Joao Marcos <joaomarcosgol2000@gmail.com>
2024-10-13 09:30:55 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/uespi/autoescola/AutoescolaApplication.java
@@ -0,0 +1,13 @@
+package br.uespi.autoescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoescolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoescolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uespi/autoescola/AutoescolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uespi.autoescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoescolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
