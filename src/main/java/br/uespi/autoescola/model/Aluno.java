02874df6e96acf9abb5f0637027ce04114a3eeb1
Joao Marcos <joaomarcosgol2000@gmail.com>
2024-10-13 10:15:12 -0300

    Implementação dos getters e setters na classe Aluno

--- src/main/java/br/uespi/autoescola/model/Aluno.java
+++ src/main/java/br/uespi/autoescola/model/Aluno.java
@@ -15,4 +15,43 @@ public class Aluno {
     private Date dataDeNascimento;
     private String telefone;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
