bf2b4cee51de656cda66982ac157684c38d3da05
Joao Marcos <joaomarcosgol2000@gmail.com>
2024-10-13 19:15:32 -0300

    Criando o serviço de gerenciamento de Instrutor

---
+++ src/main/java/br/uespi/autoescola/service/InstrutorService.java
@@ -0,0 +1,31 @@
+package br.uespi.autoescola.service;
+
+import br.uespi.autoescola.model.Instrutor;
+import br.uespi.autoescola.repository.InstrutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstrutorService {
+
+    @Autowired
+    private InstrutorRepository instrutorRepository;
+
+    public List<Instrutor> findAll() {
+        return instrutorRepository.findAll();
+    }
+
+    public Instrutor findById(Long id) {
+        return instrutorRepository.findById(id).orElse(null);
+    }
+
+    public Instrutor save(Instrutor instrutor) {
+        return instrutorRepository.save(instrutor);
+    }
+
+    public void deleteById(Long id) {
+        instrutorRepository.deleteById(id);
+    }
+}
