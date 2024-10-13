4d0f53306a4647b1e8686ee06ceeb35251bd0ed8
Joao Marcos <joaomarcosgol2000@gmail.com>
2024-10-13 10:25:36 -0300

    Implementação dos getters e setters na classe Veiculo

--- src/main/java/br/uespi/autoescola/model/Veiculo.java
+++ src/main/java/br/uespi/autoescola/model/Veiculo.java
@@ -13,5 +13,36 @@ public class Veiculo {
     private String modelo;
     private Integer ano;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
 }
 
