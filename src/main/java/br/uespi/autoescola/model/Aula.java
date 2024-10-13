a5152b17a6731ccf35cba2dab11a0fcc5e2216cd
Joao Marcos <joaomarcosgol2000@gmail.com>
2024-10-13 10:12:37 -0300

    Implementação dos getters e settes na classe Aula

--- src/main/java/br/uespi/autoescola/model/Aula.java
+++ src/main/java/br/uespi/autoescola/model/Aula.java
@@ -14,4 +14,35 @@ public class Aula {
     private String hora;
     private String local;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
 }
